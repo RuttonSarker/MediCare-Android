652ea286764e09538bbcc3d5985bb97f998792f0
Rutton Sarker <ruttonchandrasarker@gmail.com>
2021-03-02 19:24:12 +0600

    LoginActivity,PatientActivity,others drawable and res file added

--- app/src/main/java/com/redbox/medicare/LoginActivity.java
+++ app/src/main/java/com/redbox/medicare/LoginActivity.java
@@ -2,13 +2,213 @@ package com.redbox.medicare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.ServerError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.redbox.medicare.UtilsService.SharedPreference;
+import com.redbox.medicare.UtilsService.UtilService;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText mobileNumber, password;
+    private Button login;
+    private TextView signup;
+    private Spinner user;
+    UtilService utilService;
+    private String mobile, pass, userType;
+    CharSequence[] UserType;
+    SharedPreference sharedPreference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mobileNumber = findViewById(R.id.mobileNumber);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+        signup = findViewById(R.id.signup);
+        user = findViewById(R.id.user);
+        utilService = new UtilService();
+        sharedPreference = new SharedPreference(this);
+
+        UserType = getResources().getStringArray(R.array.User_Type);
+        ArrayAdapter myUserType = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, UserType);
+        myUserType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        user.setAdapter(myUserType);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, DoctorList.class));
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                utilService.hideKeyboard(view, LoginActivity.this);
+                mobile = mobileNumber.getText().toString();
+                pass = password.getText().toString();
+                userType = user.getSelectedItem().toString();
+                if (validate(view)) {
+                    loginUser(view);
+                }
+
+            }
+        });
     }
+
+    private void loginUser(View view) {
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Please Wait...");
+        progressDialog.show();
+
+        final HashMap<String, String> params = new HashMap<>();
+        params.put("usertype", userType);
+        params.put("mobile", mobile);
+        params.put("password", pass);
+
+
+        String apiKey = "http://192.168.31.122:3000/api/medicare/auth/login";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
+                apiKey, new JSONObject(params), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    if (response.getBoolean("success")) {
+                        String token = response.getString("token");
+                        sharedPreference.setValueString("token", token);
+                        String userType = response.getString("usertype");
+                        sharedPreference.setUserString("usertype", userType);
+
+                        if (userType.equals("Doctor")) {
+                            Intent intent = new Intent(LoginActivity.this, DoctorActivity.class);
+                            startActivity(intent);
+
+                        } else if (userType.equals("Patient")) {
+                            startActivity(new Intent(LoginActivity.this, PatientActivity.class));
+
+                        }
+                    }
+                    progressDialog.dismiss();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    progressDialog.dismiss();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                NetworkResponse response = error.networkResponse;
+                if (error instanceof ServerError && response != null) {
+                    try {
+                        String res = new String(response.data, HttpHeaderParser.parseCharset(response.headers, "utf-8"));
+                        JSONObject obj = new JSONObject(res);
+                        Toast.makeText(LoginActivity.this, obj.getString("msg"), Toast.LENGTH_SHORT).show();
+                        progressDialog.dismiss();
+                    } catch (JSONException | UnsupportedEncodingException je) {
+                        je.printStackTrace();
+                        progressDialog.dismiss();
+
+                    }
+                }
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<>();
+                headers.put("Content-Type", "application/json");
+                return params;
+            }
+        };
+
+        // set retry policy
+        int socketTime = 3000;
+        RetryPolicy policy = new DefaultRetryPolicy(socketTime,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        jsonObjectRequest.setRetryPolicy(policy);
+
+        // request add
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonObjectRequest);
+    }
+
+
+    public boolean validate(View view) {
+        boolean isValid;
+
+        if (!TextUtils.isEmpty(mobile)) {
+
+            if (!TextUtils.isEmpty(pass)) {
+                isValid = true;
+            } else {
+                utilService.showSnackBar(view, "Password Empty! Enter Password");
+                isValid = false;
+            }
+        } else {
+            utilService.showSnackBar(view, "Mobile No. Empty! Enter Mobile No.");
+            isValid = false;
+        }
+
+        return isValid;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        SharedPreferences medicare = getSharedPreferences("medicare_user", MODE_PRIVATE);
+        String user = sharedPreference.getUserString("usertype");
+
+        if (medicare.contains("token")) {
+
+            if (user.equals("Doctor")) {
+                startActivity(new Intent(LoginActivity.this, DoctorActivity.class));
+                finish();
+
+            }else if(user.equals("Patient"))
+                startActivity(new Intent(LoginActivity.this, PatientActivity.class));
+                finish();
+        }
+
+    }
+
 }
+
+
--- app/src/main/java/com/redbox/medicare/PatientActivity.java
+++ app/src/main/java/com/redbox/medicare/PatientActivity.java
@@ -1,15 +1,33 @@
 package com.redbox.medicare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class PatientActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+import com.redbox.medicare.UtilsService.SharedPreference;
+
+public class PatientActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private CardView doctor,appointment,prescription,report;
+    ImageView menuTop;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    LinearLayout contentView;
+    static final float END_SCALE = 0.7f;
+    SharedPreference sharedPreference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +37,18 @@ public class PatientActivity extends AppCompatActivity {
         appointment = findViewById(R.id.appointment);
         prescription = findViewById(R.id.prescription);
         report = findViewById(R.id.medicalReport);
+        drawerLayout = findViewById(R.id.drawer_patient_layout);
+        navigationView = findViewById(R.id.navigation_patient);
+        menuTop = findViewById(R.id.menuTop);
+        contentView = findViewById(R.id.contentView);
+        sharedPreference = new SharedPreference(this);
+
+        navigationDrawer();
 
         doctor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(PatientActivity.this,DoctorList.class));
+                startActivity(new Intent(PatientActivity.this,ListDoctor.class));
             }
         });
 
@@ -48,6 +73,106 @@ public class PatientActivity extends AppCompatActivity {
             }
         });
 
+    }
 
+    private void navigationDrawer() {
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        menuTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (drawerLayout.isDrawerVisible(GravityCompat.START))
+                    drawerLayout.closeDrawer(GravityCompat.START);
+
+                else drawerLayout.openDrawer(GravityCompat.START);
+            }
+
+        });
+        animateNavigationDrawer();
+        MyMenu();
+    }
+
+    private void MyMenu() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+                if (id == R.id.profile) {
+                    startActivity(new Intent(PatientActivity.this,PatientProfile.class));
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+                else if (id == R.id.notification) {
+                    startActivity(new Intent(PatientActivity.this,PatientNotification.class));
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+                else if (id == R.id.messege) {
+                    startActivity(new Intent(PatientActivity.this,PatientMessege.class));
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+                else if (id == R.id.feedback) {
+                    startActivity(new Intent(PatientActivity.this,Feedback.class));
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+                else if (id == R.id.about) {
+                    startActivity(new Intent(PatientActivity.this,AboutUs.class));
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+                else if (id == R.id.logout){
+                    sharedPreference.clear();
+                    startActivity(new Intent(PatientActivity.this,LoginActivity.class));
+                    finish();
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+
+                return false;
+            }
+
+        });
+    }
+
+    private void animateNavigationDrawer() {
+        //Add any color or remove it to use the default one!
+        //To make it transparent use Color.Transparent in side setScrimColor();
+        //drawerLayout.setScrimColor(Color.TRANSPARENT);
+        drawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+                // Scale the View based on current slide offset
+                final float diffScaledOffset = slideOffset * (1 - END_SCALE);
+                final float offsetScale = 1 - diffScaledOffset;
+                contentView.setScaleX(offsetScale);
+                contentView.setScaleY(offsetScale);
+
+                // Translate the View, accounting for the scaled width
+                final float xOffset = drawerView.getWidth() * slideOffset;
+                final float xOffsetDiff = contentView.getWidth() * diffScaledOffset / 2;
+                final float xTranslation = xOffset - xOffsetDiff;
+                contentView.setTranslationX(xTranslation);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+
+            drawerLayout.closeDrawer(GravityCompat.START);
+
+        } else super.onBackPressed();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
     }
 }
