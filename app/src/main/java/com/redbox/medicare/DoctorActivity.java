c99a4194426606ab485eb9a27e9ec3c14cd4d740
Rutton Sarker <ruttonchandrasarker@gmail.com>
2021-03-08 17:29:03 +0600

    DoctorActivity, UtilService, layout added

--- app/src/main/java/com/redbox/medicare/DoctorActivity.java
+++ app/src/main/java/com/redbox/medicare/DoctorActivity.java
@@ -1,52 +1,183 @@
 package com.redbox.medicare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-public class DoctorActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+import com.redbox.medicare.UtilsService.SharedPreference;
 
-    private CardView schedule,appointment,prescription,report;
+public class DoctorActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private CardView schedule, appointment, prescription, report;
+    ImageView menuTop;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    LinearLayout contentView;
+    static final float END_SCALE = 0.7f;
+    SharedPreference sharedPreference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_doctor);
         schedule = findViewById(R.id.schedule);
         appointment = findViewById(R.id.appointment);
         prescription = findViewById(R.id.prescription);
         report = findViewById(R.id.medicalReport);
+        drawerLayout = findViewById(R.id.drawer_doctor_layout);
+        navigationView = findViewById(R.id.navigation_doctor);
+        menuTop = findViewById(R.id.menuTop);
+        contentView = findViewById(R.id.contentView);
+        sharedPreference = new SharedPreference(this);
+
+        navigationDrawer();
+
 
         schedule.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(DoctorActivity.this,ScheduleManager.class));
+                startActivity(new Intent(DoctorActivity.this, ScheduleManager.class));
             }
         });
 
         appointment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(DoctorActivity.this,AppointmentList.class));
+                startActivity(new Intent(DoctorActivity.this, AppointmentList.class));
             }
         });
 
         prescription.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(DoctorActivity.this,PrescriptionRecord.class));
+                startActivity(new Intent(DoctorActivity.this, PrescriptionRecord.class));
             }
         });
 
         report.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(DoctorActivity.this,MedicalReportList.class));
+                startActivity(new Intent(DoctorActivity.this, MedicalReportList.class));
             }
         });
 
+    }
+
+    private void navigationDrawer() {
+
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        menuTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (drawerLayout.isDrawerVisible(GravityCompat.START))
+                    drawerLayout.closeDrawer(GravityCompat.START);
+
+                else drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        animateNavigationDrawer();
+        MyMenu();
+
+    }
+
+    private void MyMenu() {
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.profile) {
+                    startActivity(new Intent(DoctorActivity.this,DoctorProfile.class));
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+                else if (id == R.id.notification) {
+                    startActivity(new Intent(DoctorActivity.this,DoctorNotification.class));
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+                else if (id == R.id.messege) {
+                    startActivity(new Intent(DoctorActivity.this,DoctorMessege.class));
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+                else if (id == R.id.feedback) {
+                    startActivity(new Intent(DoctorActivity.this,Feedback.class));
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+                else if (id == R.id.about) {
+                    startActivity(new Intent(DoctorActivity.this,AboutUs.class));
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+                else if (id == R.id.logout){
+                    sharedPreference.clear();
+                    startActivity(new Intent(DoctorActivity.this,LoginActivity.class));
+                    finish();
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+
+                }
+
+                return false;
+            }
+
+        });
+    }
+
+    private void animateNavigationDrawer() {
+        //Add any color or remove it to use the default one!
+        //To make it transparent use Color.Transparent in side setScrimColor();
+        //drawerLayout.setScrimColor(Color.TRANSPARENT);
+        drawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+                // Scale the View based on current slide offset
+                final float diffScaledOffset = slideOffset * (1 - END_SCALE);
+                final float offsetScale = 1 - diffScaledOffset;
+                contentView.setScaleX(offsetScale);
+                contentView.setScaleY(offsetScale);
+
+                // Translate the View, accounting for the scaled width
+                final float xOffset = drawerView.getWidth() * slideOffset;
+                final float xOffsetDiff = contentView.getWidth() * diffScaledOffset / 2;
+                final float xTranslation = xOffset - xOffsetDiff;
+                contentView.setTranslationX(xTranslation);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+
+            drawerLayout.closeDrawer(GravityCompat.START);
+
+        } else super.onBackPressed();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        return true;
     }
 }
