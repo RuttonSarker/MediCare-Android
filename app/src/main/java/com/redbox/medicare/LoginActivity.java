87ea4f69a5c3014270095bb1d577d1fdaadaf50f
Rutton Sarker <ruttonchandrasarker@gmail.com>
2021-05-15 23:20:51 +0600

    updated

--- app/src/main/java/com/redbox/medicare/LoginActivity.java
+++ app/src/main/java/com/redbox/medicare/LoginActivity.java
@@ -72,7 +72,7 @@ public class LoginActivity extends AppCompatActivity {
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, DoctorList.class));
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             }
         });
 
