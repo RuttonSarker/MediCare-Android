7d693acb496dd09faaaebef7b3bcbcf9b0aa6c8f
Rutton Sarker <ruttonchandrasarker@gmail.com>
2021-05-24 21:40:38 +0600

    DailyReportFragment added

---
+++ app/src/main/java/com/redbox/medicare/Fragments/DailyReportFragment.java
@@ -0,0 +1,304 @@
+package com.redbox.medicare.Fragments;
+
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.ServerError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.redbox.medicare.DailyReport;
+import com.redbox.medicare.DailyReportAdapter;
+import com.redbox.medicare.DoctorAdapter;
+import com.redbox.medicare.FindDoctor;
+import com.redbox.medicare.MainActivity;
+import com.redbox.medicare.PatientProfile;
+import com.redbox.medicare.R;
+import com.redbox.medicare.UtilsService.SharedPreference;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class DailyReportFragment extends Fragment {
+
+    FloatingActionButton floatingActionButton;
+    private String token;
+    private SharedPreference sharedPreference;
+    RecyclerView recyclerView;
+    ProgressBar progressBar;
+    DailyReportAdapter dailyReportAdapter;
+    ArrayList<DailyReport> arrayList;
+
+    public DailyReportFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_daily_report, container, false);
+
+        floatingActionButton = view.findViewById(R.id.addButton);
+        sharedPreference = new SharedPreference(getContext());
+        token = sharedPreference.getValueString("token");
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                showAlertDialog();
+
+            }
+        });
+
+        recyclerView = view.findViewById(R.id.recycle_view);
+        progressBar = view.findViewById(R.id.progressBar);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setHasFixedSize(true);
+        getReport();
+
+        return view;
+    }
+
+    public void showAlertDialog(){
+
+        LayoutInflater inflater = getLayoutInflater();
+        View alertLayout = inflater.inflate(R.layout.custom_daily_report_layout, null);
+
+        final EditText date = alertLayout.findViewById(R.id.date);
+        final EditText bloodPressure = alertLayout.findViewById(R.id.pressure);
+        final EditText diabetesRange = alertLayout.findViewById(R.id.diabetes);
+
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getLayoutInflater().getContext(),
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                 date.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+
+                datePickerDialog.show();
+            }
+        });
+
+        final AlertDialog dialog = new AlertDialog.Builder(getActivity())
+                .setView(alertLayout)
+                .setTitle("")
+                .setPositiveButton("Add", null)
+                .setNegativeButton("Cancel", null)
+                .create();
+
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+                Button button = ((AlertDialog)dialog).getButton(AlertDialog.BUTTON_POSITIVE);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String datePick = date.getText().toString();
+                        String pressureTest = bloodPressure.getText().toString();
+                        String diabetesTest = diabetesRange.getText().toString();
+                        AddReport(datePick, pressureTest, diabetesTest);
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+        dialog.show();
+    }
+
+    public void AddReport(String datePick, String pressureTest, String diabetesTest){
+
+        final HashMap<String, String> body = new HashMap<>();
+        body.put("date", datePick);
+        body.put("bloodPressure", pressureTest);
+        body.put("diabetesTest", diabetesTest);
+
+        String apiKey = "http://192.168.31.122:3000/api/medicare/auth/report/insert";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
+                apiKey, new JSONObject(body), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    if (response.getBoolean("success")) {
+
+                        Toast.makeText(getActivity(), "Added Successfully", Toast.LENGTH_SHORT).show();
+                        getReport();
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                NetworkResponse response = error.networkResponse;
+                if (error instanceof ServerError && response != null) {
+                    try {
+                        String res = new String(response.data, HttpHeaderParser.parseCharset(response.headers, "utf-8"));
+                        JSONObject obj = new JSONObject(res);
+                        Toast.makeText(getActivity(), obj.getString("msg"), Toast.LENGTH_SHORT).show();
+
+                    } catch (JSONException | UnsupportedEncodingException je) {
+                        je.printStackTrace();
+
+
+                    }
+                }
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<>();
+                headers.put("Content-Type", "application/json");
+                headers.put("Authorization", token);
+                return headers;
+            }
+        };
+
+        // set retry policy
+        int socketTime = 3000;
+        RetryPolicy policy = new DefaultRetryPolicy(socketTime,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        jsonObjectRequest.setRetryPolicy(policy);
+
+        // request add
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(jsonObjectRequest);
+
+    }
+
+    public void getReport(){
+
+        arrayList = new ArrayList<>();
+        progressBar.setVisibility(View.VISIBLE);
+        String Url = "http://192.168.31.122:3000/api/medicare/auth/report/get";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                Url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    if (response.getBoolean("success")) {
+
+                        JSONArray jsonArray = response.getJSONArray("user");
+
+                        for (int i = 0; i < jsonArray.length(); i++){
+
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                           DailyReport dailyReport = new DailyReport(
+                                   jsonObject.getString("date"),
+                                   jsonObject.getString("bloodPressure"),
+                                   jsonObject.getString("diabetesTest")
+
+                           );
+
+                            arrayList.add(dailyReport);
+                        }
+
+                        dailyReportAdapter = new DailyReportAdapter(getActivity(),arrayList,DailyReportFragment.this);
+                        recyclerView.setAdapter(dailyReportAdapter);
+
+                    }
+                    progressBar.setVisibility(View.GONE);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getActivity(), error.toString(), Toast.LENGTH_SHORT).show();
+                NetworkResponse response = error.networkResponse;
+                if (error instanceof ServerError && response != null) {
+                    try {
+                        String res = new String(response.data, HttpHeaderParser.parseCharset(response.headers, "utf-8"));
+                        JSONObject obj = new JSONObject(res);
+                        Toast.makeText(getActivity(), obj.getString("msg"), Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.GONE);
+
+                    } catch (JSONException | UnsupportedEncodingException je) {
+                        je.printStackTrace();
+                        progressBar.setVisibility(View.GONE);
+
+                    }
+                }
+                progressBar.setVisibility(View.GONE);
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<>();
+                headers.put("Content-Type", "application/json");
+                headers.put("Authorization", token);
+                return headers;
+            }
+        };
+
+        // set retry policy
+        int socketTime = 3000;
+        RetryPolicy policy = new DefaultRetryPolicy(socketTime, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        jsonObjectRequest.setRetryPolicy(policy);
+
+        // request add
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(jsonObjectRequest);
+    }
+
+}
