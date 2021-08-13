4d062c156a742c80124301c335f2a2f3e96cfb04
Rutton Sarker <ruttonchandrasarker@gmail.com>
2021-08-14 00:29:05 +0600

    drawable file

---
+++ app/src/main/java/com/redbox/medicare/PatientMessege.java
@@ -0,0 +1,14 @@
+package com.redbox.medicare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientMessege extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_messege);
+    }
+}
